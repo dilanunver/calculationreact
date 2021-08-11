c0a53d07286611c3419188d8af0c51749f1cb571
dilanunver <dunver33@gmail.com>
2021-08-11 17:40:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App (){
+
+  return ( 
+    <>
+   
+    <input type="text" value="10"></input>
+    <div className="position">
+ 
+  <button className="grey">C</button>
+  <button className="grey">+/-</button>
+  <button className="grey">%</button>
+  <button className="orange">÷</button>
+  <button>7</button>
+  <button>8</button>
+  <button>9</button>
+  <button className="orange">×</button>
+  <button>4</button>
+  <button>5</button>
+  <button>6</button>
+  <button className="orange">-</button>
+  <button>1</button>
+  <button>2</button>
+  <button>3</button>
+  <button className="orange">+</button>
+  <button className="header">0</button>
+  <button>,</button>
+  <button className="orange">=</button>
+  </div>
+  </>
+  )
 }
 
-export default App;
+export default App; 
